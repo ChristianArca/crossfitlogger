4210ceea0b86b1e101899ab98a04f581dc77fb82
Christian Arca <chris.arca@gmail.com>
2014-02-01 18:39:16 -0500

    Cleaning things up

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-<script type="text/javascript">
-			console.log("In Javascript");
-			var url  = "https://christianarca.firebaseio.com/movement_list";
-			var myDataRef = new Firebase(url);
+console.log("In Javascript");
+var url = "https://christianarca.firebaseio.com/";
+var movementsRef  = "movements";
+var myDataRef = new Firebase(url);
 
-			function eventManager(event) {
-				console.log("In event manager");
-				var movement = $('#movementInput').val();
-				var date = Date();
-				var onComplete = function(error) {
-					if (error) {
-						alert("Data not saved" + error);
-					} else {
-						alert("Data saved");
-					}
-				};
-				myDataRef.push({movement: movement, date: date}, onComplete);
-				console.log("The movement is:"+movement);
-			};
-			var submit = document.getElementsByTagName('button')[0];
-			submit.onclick = eventManager;
-		</script>
+console.log(url+movementsRef);
 
+function eventManager(event) {
+	console.log("In event manager");
+	var movement = $('#movementInput').val();
+	var date = Date();
+	var onComplete = function(error) {
+		if (error) {
+			alert("Data not saved" + error);
+		} else {
+			alert("Data saved");
+		}
+	};
+	myDataRef.push({movement: movement, date: date}, onComplete);
+	console.log("The movement is:"+movement);
+};
 
+var submit = document.getElementsByTagName('button')[0];
+submit.onclick = eventManager;
 
-
-
-
-
-		TESTING 
+//Commit trouble so I'm writing a comment
