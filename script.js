b497cfa8af7cad3f12aabc09f52fb8b8af49f10c
Christian Arca <chris.arca@gmail.com>
2014-02-01 19:47:16 -0500

    Changing the way I write data to Firebase.

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-console.log("In Javascript");
-var url = "https://christianarca.firebaseio.com/";
-var movementsRef  = "movements";
-var myDataRef = new Firebase(url);
 
-console.log(url+movementsRef);
+console.log(document.URL);
+var url = "https://christianarca.firebaseio.com/";
+var movementsRef  = url+"movements";
+var myDataRef = new Firebase(url);
+var test = "test";
 
 function eventManager(event) {
-	console.log("In event manager");
+	console.log("In eventManager");
 	var movement = $('#movementInput').val();
 	var date = Date();
 	var onComplete = function(error) {
 		if (error) {
-			alert("Data not saved" + error);
+			console.log("Data not saved" + error);
 		} else {
-			alert("Data saved");
+			console.log("Data saved");
 		}
-	};
-	myDataRef.push({movement: movement, date: date}, onComplete);
-	console.log("The movement is:"+movement);
+	}
+	var inputRef = new Firebase(movementsRef+"/"+movement);
+	inputRef.push({name: movement, created: date})
 };
 
 var submit = document.getElementsByTagName('button')[0];
-submit.onclick = eventManager;
-
-//Commit trouble so I'm writing a comment
