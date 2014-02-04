7f8da66287a776a8cacd0a1a76c540fafb2b8b78
Christian Arca <chris.arca@gmail.com>
2014-02-03 22:25:34 -0500

    Trying to get onclick to work in JS

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 
-console.log(document.URL);
 var url = "https://christianarca.firebaseio.com/";
-var movementsRef  = url+"movements";
 var myDataRef = new Firebase(url);
-var test = "test";
+var movementsRef  = url+"movements";
 
 function eventManager(event) {
-	console.log("In eventManager");
 	var movement = $('#movementInput').val();
 	var date = Date();
-	var onComplete = function(error) {
-		if (error) {
-			console.log("Data not saved" + error);
-		} else {
-			console.log("Data saved");
-		}
-	}
-	var inputRef = new Firebase(movementsRef+"/"+movement);
-	inputRef.push({name: movement, created: date})
-};
+	
+	movementsRef.push({name: movement, created: date});
+	event.preventDefault();
+}
 
 var submit = document.getElementsByTagName('button')[0];
+submit.onclick = eventManager;
